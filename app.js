f8b3a69ffc5b6589d896a227b59be02e6d2e6071
Taha Khan <kyle@thebasement.tv>
2016-08-17 16:40:55 -0400

    remove sensitive info

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var Discord = require( 'discord.js' );
 
 var bot = new Discord.Client();
 
-bot.loginWithToken( 'MjE1MTkxMDk3NjQ1NzI3NzQ1.CpZGVQ.J9hIKBBStBpP69X3qGAHfDOkOTY', function( err, token ){
+bot.loginWithToken( 'YOUR_TOKEN_HERE', function( err, token ){
 	if( err ){
 		console.log( 'There was an error loggin in: ' + error );
 		return;
